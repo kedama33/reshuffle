9a64e4ab92d4cc2d6114a98118ed716a7ee20013
kedama <>
2025-01-04 00:13:38 +0900

    入力されたファイルの拡張子がcsvであるか否かのチェックを追加

--- src/utils/InputUtil.java
+++ src/utils/InputUtil.java
@@ -19,12 +19,20 @@ public class InputUtil {
   }
 
   public boolean checkInputCSVFilePath() {
-    return checkBlank();
+    return checkBlank() || checkExtension() ? true : false;
   }
 
   boolean checkBlank() {
     if (csvFilePath.isBlank() || csvFilePath.isEmpty()) {
       System.out.println("※※※入力されたパスがブランクです※※※");
+      return true;
+    }
+    return false;
+  }
+
+  boolean checkExtension() {
+    if (csvFilePath.length() < 4 || !csvFilePath.substring(csvFilePath.length() - 4).equals(".csv")) {
+      System.out.println("※※※入力されたファイルがcsvではありません※※※");
       return true;
     }
     return false;
